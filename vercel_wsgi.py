fe0f2e1cff47c5a5b0b402e2f6431cdedc773de5
AdityaKO2 <aditya123906@gmail.com>
2024-12-05 21:37:52 +0530

    wshiii

--- vercel_wsgi.py
+++ vercel_wsgi.py
@@ -1,3 +1,6 @@
-from django.core.wsgi import get_wsgi_application
+import os
+from django.core.asgi import get_asgi_application
 
-application = get_wsgi_application()
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'your_project_name.settings')
+
+application = get_asgi_application()
